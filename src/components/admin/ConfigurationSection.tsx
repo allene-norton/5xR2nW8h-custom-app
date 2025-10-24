9fdfda532b5afd2493c7d30a852e35ec21f2294e
Allene Norton <norton.allene@gmail.com>
2025-10-24 14:15:38 -0400

    comment for customFields

--- src/components/admin/ConfigurationSection.tsx
+++ src/components/admin/ConfigurationSection.tsx
@@ -87,7 +87,7 @@ export function ConfigurationSection({
       city: selectedClient.customFields?.city || '',
       state: selectedClient.customFields?.state || '',
       postalCode: selectedClient.customFields?.postalCode || '',
-      birthdate: selectedClient.customFields?.birthdate || '',
+      birthdate: selectedClient.customFields?.birthdate || '', //birthDate in CT fields
     };
 
     updateIdentification(updatedIdentification);
