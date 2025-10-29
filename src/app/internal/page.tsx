d607f576131505df616ab3504bd4b60d017d7912
Allene Norton <norton.allene@gmail.com>
2025-10-29 16:14:50 -0400

    remove log

--- src/app/internal/page.tsx
+++ src/app/internal/page.tsx
@@ -30,7 +30,7 @@ interface InternalPageProps {
 
 export default function InternalPage({ searchParams }: InternalPageProps) {
   // console.log(`SEARCHPARAMS`, searchParams)
-  console.log(`token`, searchParams.token)
+  // console.log(`token`, searchParams.token)
   // ----------- STATES-------------------------------------------
   // CLIENTS STATES
   const [clientsResponse, setClientsResponse] = useState<ListClientsResponse>({
